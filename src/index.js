e9199306e04ea54d10d64950d4cf317be565dd12
alexbotha <alexbotha@hotmail.co.uk>
2022-08-02 12:58:50 +0100

    commit tree 6151e47bcaf9eb9523832a17025577a299eb89fd

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
-const init = () => {
-  
-}
+document.addEventListener('DOMContentLoaded', () => {
+  const inputForm = document.querySelector('form')
 
-document.addEventListener('DOMContentLoaded', init);
+  inputForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const c = document.querySelector('input#searchByID');
+
+    fetch(`http://localhost:3000/movies/${c.value}`)
+    .then(response => response.json())
+    .then(data => {
+      const title = document.querySelector('section#movieDetails h4');
+      const summary = document.querySelector('section#movieDetails p');
+
+      title.innerText = data.title;
+      summary.innerText = data.summary;
+    });
+  });
+})
